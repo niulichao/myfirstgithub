c9bf91a72467e0b2a0878c0b8544540803943169
niulichao <511060153@qq.com>
2018-12-06 10:08:20 +0800

    Hello GitHub2

---
+++ GitTest/src/main/java/test/GitTest2.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by nlc on 2018/12/6.
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
